b55ddb6371db674be38428745d2d121e202818f0
Marcus Mathuram <marcus.mathuram7@gmail.com>
2024-06-27 23:45:50 +0530

    env completed

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,11 +1,38 @@
 import React from 'react'
+import { BiPowerOff } from 'react-icons/bi';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import { logoutRoute } from '../utils/ApiRoutes';
+import axios from 'axios';
 
-function Logout() {
+export default function Logout() {
+  const navigate = useNavigate();
+  const handleClick = async()=>{
+    const id = await JSON.parse(localStorage.getItem(import.meta.env.VITE_APP_LOCALHOST_KEY)._id);
+    const data = await axios.get(`${logoutRoute}/${id}`);
+    if(data.status === 200){
+      localStorage.clear();
+      navigate("/login");
+    }
+  }
   return (
-    <div>
-      
-    </div>
+    <Button onClick={handleClick}>
+      <BiPowerOff />
+    </Button>
   )
 }
 
-export default Logout
+const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+  background-color: #9a86f3;
+  border: none;
+  cursor: pointer;
+  svg {
+    font-size: 1.3rem;
+    color: #ebe7ff;
+  }
+`;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,38 +1,90 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import Logo from "../assets/logo.svg";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import { registerRoute } from '../utils/ApiRoutes.js';
 export default function Register() {
+  const navigate = useNavigate();
   const [values, setValues]= useState({
     username: "",
     email: "",
     password: "",
     confirmPassword: "",
   })
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    alert("form");
-  };
-
-  const handleValidation = () =>{
-    const { password, confirmPassword, username, email } = values;
-    if (password!== confirmPassword) {
-      alert("Passwords do not match");
-      return false;
-    }
+  const toastOptions = {
+    position: "bottom-right",
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
   }
+
+  useEffect(()=>{
+    if(localStorage.getItem(import.meta.env.VITE_APP_LOCALHOST_KEY)){
+      navigate("/")
+    }
+  }, [])
 
   const handleChange = (event) => {
     setValues({ ...values, [event.target.name]: event.target.value });
   };
+
+  const handleValidation = () =>{
+    const { password, confirmPassword, username, email } = values;
+    if (password !== confirmPassword) {
+      toast.error(
+        "Password and confirm password should be same.", toastOptions
+      );
+      return false;
+    } else if (username.length <3){
+      toast.error(
+        "Username should be atleast 3 characters long.", toastOptions
+      );
+      return false;
+    } else if (password.length < 8) {
+      toast.error(
+        "Password should be atleast 8 characters long.", toastOptions
+      );
+      return false;
+    } else if (email === "") {
+      toast.error(
+        "Email is required.", toastOptions
+      );
+      return false;
+    }
+    return true;
+  }
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (handleValidation()) {
+      const { email, username, password } = values;
+      const { data } = await axios.post(registerRoute, {
+        username,
+        email,
+        password,
+      })
+      if (data.status === false) {
+        toast.error(data.msg, toastOptions);
+      }
+      if (data.status === true) {
+        localStorage.setItem(
+          import.meta.env.VITE_APP_LOCALHOST_KEY,
+          JSON.stringify(data.user)
+        );
+        navigate("/");
+      }
+    }
+  };
   return (
     <>
       <FormContainer>
-        <form onSubmit={(event) => handleSubmit(event)}>
+        <form action="" onSubmit={(event) => handleSubmit(event)}>
           <div className="brand">
-            <img src={Logo} alt="" />
+            <img src={Logo} alt="logo" />
             <h1>Rehoboth</h1>
           </div>
           <input
@@ -65,6 +117,7 @@ export default function Register() {
           </span>
         </form>
       </FormContainer>
+      <ToastContainer />
     </>
   );
 }
@@ -117,6 +170,7 @@ const FormContainer = styled.div`
     color: white;
     padding: 1rem 2rem;
     border: none;
+    font-weight: bold;
     border-radius: 0.4rem;
     font-size: 1rem;
     cursor: pointer;
